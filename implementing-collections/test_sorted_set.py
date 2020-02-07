8661bbf7e68b5c21da371dd014747336a17101d5
unknown <pmichelbrink@gmail.com>
2020-02-07 12:53:31 -0700

    constructor tests

--- implementing-collections/test_sorted_set.py
+++ implementing-collections/test_sorted_set.py
@@ -24,5 +24,42 @@ class TestConstruction(unittest.TestCase):
     def test_default_empty(self):
         s = SortedSet()
         
+
+class TestContainerProtocol(unittest.TestCase):
+
+    def setUp(self):
+        self.s = SortedSet([6,7,3,9])
+
+    def test_positive_contained(self):
+        self.assertTrue(6 in self.s)
+
+    def test_negative_contained(self):
+        self.assertFalse(2 in self.s)
+
+    def test_positive_not_contained(self):
+        self.assertTrue(5 not in self.s)
+
+    def test_negative_not_contained(self):
+        self.assertFalse(9 not in self.s)
+
+
+class TestSizedProtocol(unittest.TestCase):
+
+    def test_empty(self):
+        s = SortedSet()
+        self.assertEqual(len(s), 0)
+
+    def test_one(self):
+        s = SortedSet([42])
+        self.assertEqual(len(s), 1)
+
+    def test_ten(self):
+        s = SortedSet(range(10))
+        self.assertEqual(len(s), 10)
+
+    def test_with_duplicates(self):
+        s = SortedSet([5,5,5])
+        self.assertEqual(len(s), 1)
+
 if __name__ == '__main__':
     unittest.main()
