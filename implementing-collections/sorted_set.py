633c24eca6af46207aa2a91b5a4df152f51405d6
pauladmin <31351276+pmichelbrink@users.noreply.github.com>
2020-02-17 16:39:19 -0700

    more test cases

--- implementing-collections/sorted_set.py
+++ implementing-collections/sorted_set.py
@@ -9,3 +9,16 @@ class SortedSet:
     def __len__(self):
         return len(self._items)
 
+    def __iter__(self):
+        return iter(self._items)
+
+    def __getitem__(self, index):
+        result = self._items[index]
+        return SortedSet(result) if isinstance(index, slice) else result
+
+    def __repr__(self):
+        return "SortedSet({})".format(
+            repr(self._items) if self._items else ''
+        )
+
+    
--- implementing-collections/test_sorted_set.py
+++ implementing-collections/test_sorted_set.py
@@ -61,5 +61,74 @@ class TestSizedProtocol(unittest.TestCase):
         s = SortedSet([5,5,5])
         self.assertEqual(len(s), 1)
 
+class TestIterableProtocol(unittest.TestCase):
+
+    def setUp(self):
+        self.s = SortedSet([7,2,1,1,9])
+
+    def test_iter(self):
+        i = iter(self.s)
+        self.assertEqual(next(i), 1)
+        self.assertEqual(next(i), 2)
+        self.assertEqual(next(i), 7)
+        self.assertEqual(next(i), 9)
+        self.assertRaises(StopIteration, lambda: next(i))
+
+    def test_for_loop(self):
+        index = 0
+        expected = [1,2,7,9]
+        for item in self.s:
+            self.assertEqual(item, expected[index])
+            index += 1
+
+class TestSequenceProtocol(unittest.TestCase):
+
+    def setUp(self):
+        self.s = SortedSet([1,4,9,13,15])
+
+    def test_index_zero(self):
+        self.assertEqual(self.s[0], 1)
+    
+    def test_index_four(self):
+        self.assertEqual(self.s[4], 15)
+    
+    def test_one_beyond_the_end(self):
+        with self.assertRaises(IndexError):
+            self.s[5]
+
+    def test_index_minus_one(self):
+        self.assertEqual(self.s[-1], 15)
+
+    def test_index_minus_five(self):
+        self.assertEqual(self.s[-5], 1)
+
+    def test_index_one_before_the_beginning(self):
+        with self.assertRaises(IndexError):
+            self.s[-6]
+
+    def test_slice_from_start(self):
+        self.assertEqual(self.s[:3], SortedSet([1,4,9]))
+
+    def test_slice_to_end(self):
+        self.assertEqual(self.s[3:], SortedSet([13,15]))
+
+    def test_slice_empty(self):
+        self.assertEqual(self.s[10:], SortedSet())
+
+    def test_slice_arbitrary(self):
+        self.assertEqual(self.s[2:4], SortedSet([9,13]))
+
+    def test_slice_full(self):
+        self.assertEqual(self.s[:], self.s)
+
+class TestReprProtocol(unittest.TestCase):
+    def test_repr_empty(self):
+        s = SortedSet()
+        self.assertEqual(repr(s), "SortedSet()")
+
+    def test_repr_some(self):
+        s = SortedSet([42,40,19])
+        self.assertEqual(repr(s), "SortedSet([19, 40, 42])")
+
 if __name__ == '__main__':
     unittest.main()
